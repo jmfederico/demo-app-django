45c332dbdb70cef158642f5835330aa8c53c9b6a
Federico Jaramillo Martínez <federicojaramillom@gmail.com>
2022-01-30 13:36:59 +0100

    Extra init commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-# Run migrations automatically the first run.
+# Run migrations automatically the first time.
 # After the first time, migrations should be run manually.
 if PYTHONUNBUFFERED=0 python manage.py migrate --plan | grep --silent "contenttypes.0001_initial"
 then
     python manage.py migrate
+    python manage.py createcachetable
+    python manage.py collectstatic --noinput
 fi
 
 exec "$@"
